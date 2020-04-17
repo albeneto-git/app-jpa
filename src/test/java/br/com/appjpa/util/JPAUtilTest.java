76abb6574d4cd401f3675299dcecf7c35486c6ff
Albertino Neto <albeneto72@gmail.com>
2020-04-16 23:51:50 -0300

    Criação do arquivo persistence.xml com as propriedades para acesso ao hibernate. Criação de teste unitário para verificar a conexão.

---
+++ src/test/java/br/com/appjpa/util/JPAUtilTest.java
@@ -0,0 +1,15 @@
+package br.com.appjpa.util;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+
+public class JPAUtilTest {
+
+	@Test
+	public void getEntityManager() {
+		EntityManager sessao = JPAUtil.getEntityManager();
+		sessao.close();
+	}
+	
+}
